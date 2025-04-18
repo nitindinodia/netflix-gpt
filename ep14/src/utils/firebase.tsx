f6ebdbfa494e24754925aab2cfae533e329db341
nitindinodia <35632265+nitindinodia@users.noreply.github.com>
2025-04-18 12:46:54 +0530

    Update firebase.tsx

--- ep14/src/utils/firebase.tsx
+++ ep14/src/utils/firebase.tsx
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCjdDUzUleyw7XqtrTONobNU9lSfHn_Knw",
+  apiKey: "",
   authDomain: "netflix-33d9d.firebaseapp.com",
   projectId: "netflix-33d9d",
   storageBucket: "netflix-33d9d.firebasestorage.app",
